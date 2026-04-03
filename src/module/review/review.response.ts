6f044cf272d5876ff17c75c13f7eab06f5fcefcc
QuocThang <phanquocthang211005@gmail.com>
2026-04-03 17:07:36 +0700

    fix

--- src/module/review/review.response.ts
+++
@@ -1,9 +0,0 @@
-export interface ReviewResponseDto {
-  id: string;
-  user: any;
-  product: string;
-  rating: number;
-  comment: string;
-  images: string[];
-  createdAt: Date;
-}
--- src/socket/chat.handler.ts
+++ src/socket/chat.handler.ts
@@ -1,33 +1,33 @@
-import { IChatSocket } from "@/module/chat/chat.type";
-import { Server, Socket } from "socket.io";
+// import { IChatSocket } from "@/module/chat/chat.type";
+// import { Server, Socket } from "socket.io";
 
-export class ChatSocket implements IChatSocket {
-  private static io: Server;
+// export class ChatSocket implements IChatSocket {
+//   private static io: Server;
 
-  constructor(io: Server) {
-    ChatSocket.io = io;
-    this.initEvents();
-  }
+//   constructor(io: Server) {
+//     ChatSocket.io = io;
+//     this.initEvents();
+//   }
 
-  private initEvents() {
-    ChatSocket.io.on("connection", (socket: Socket) => {
-      // Client tham gia vào phòng chat cụ thể khi mở khung chat
-      socket.on("join_chat", (chatId: string) => {
-        socket.join(chatId);
-        console.log(`Socket ${socket.id} joined room: ${chatId}`);
-      });
+//   private initEvents() {
+//     ChatSocket.io.on("connection", (socket: Socket) => {
+//       // Client tham gia vào phòng chat cụ thể khi mở khung chat
+//       socket.on("join_chat", (chatId: string) => {
+//         socket.join(chatId);
+//         console.log(`Socket ${socket.id} joined room: ${chatId}`);
+//       });
 
-      socket.on("disconnect", () => {
-        console.log(`Socket ${socket.id} disconnected`);
-      });
-    });
-  }
+//       socket.on("disconnect", () => {
+//         console.log(`Socket ${socket.id} disconnected`);
+//       });
+//     });
+//   }
 
-  emitNewMessage(chatId: string, message: any): void {
-    ChatSocket.io.to(chatId).emit("new_message", message);
-  }
+//   emitNewMessage(chatId: string, message: any): void {
+//     ChatSocket.io.to(chatId).emit("new_message", message);
+//   }
 
-  emitMessagesRead(chatId: string, readerRole: string): void {
-    ChatSocket.io.to(chatId).emit("messages_read", { chatId, readerRole });
-  }
-}
+//   emitMessagesRead(chatId: string, readerRole: string): void {
+//     ChatSocket.io.to(chatId).emit("messages_read", { chatId, readerRole });
+//   }
+// }
