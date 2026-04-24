5c824786cb3c7657e40a7a132b7bb64af23facee
QuocThang <phanquocthang211005@gmail.com>
2026-04-24 16:34:55 +0700

    fix : deloy

--- src/config/queue.ts
+++ src/config/queue.ts
@@ -1,12 +1,16 @@
 import { Queue, QueueEvents } from "bullmq";
 import IORedis from "ioredis";
 
-const createRedisConnection = () =>
-  new IORedis(process.env.REDIS_URL || "redis://localhost:6379", {
+const createRedisConnection = () => {
+  const redisUrl = process.env.REDIS_URL || "redis://localhost:6379";
+  const isUpstashOrSecure = redisUrl.startsWith("rediss://");
+  
+  return new IORedis(redisUrl, {
     maxRetriesPerRequest: null,
     enableReadyCheck: false,
-    tls: {}, // thêm dòng này nếu dùng Upstash
+    tls: isUpstashOrSecure ? { rejectUnauthorized: false } : undefined,
   });
+};
 
 export const orderQueue = new Queue("process-checkout", {
   connection: createRedisConnection(),
